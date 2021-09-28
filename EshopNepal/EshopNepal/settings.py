84b60242c64870833ac29e26401b09fb222917d2
ashrotd <63250878+ashrotd@users.noreply.github.com>
2021-09-28 18:43:32 +0545

    update

--- EshopNepal/EshopNepal/settings.py
+++ EshopNepal/EshopNepal/settings.py
@@ -155,6 +155,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'footcric07@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ashrot@07'
+EMAIL_HOST_USER = '********'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS = True
